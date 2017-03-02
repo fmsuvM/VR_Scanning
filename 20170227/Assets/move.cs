212b83bd6c2e5415cb680ec5e6d575152c1cba73
wmrn <pepecheez@gmail.com>
2017-03-02 20:10:27 +0900

    20170227

---
+++ 20170227/Assets/move.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+    GameObject mainCamera;
+
+    // Use this for initialization
+    void Start()
+    {
+        mainCamera = GameObject.FindWithTag("MainCamera");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //mainCamera.transform.position = new Vector3(mainCamera.transform.position.x, mainCamera.transform.position.y, mainCamera.transform.position.z - 10);
+        mainCamera.transform.position += new Vector3(0.1f , 0, 0);
+        /*Vector3 pos = mainCamera.transform.position;
+        pos.x += 0.5f;
+        mainCamera.transform.position = pos;*/
+
+    }
+}
