594e0bd97d9d8f1e6531c50eca3e037a212b1672
fmsuvM <k222.tkd.fms@gmail.com>
2017-05-10 13:31:14 +0900

    initial commit

---
+++ Assets/OSCController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OSCController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// this line triggers the magic
+		OSCHandler.Instance.Init ();
+
+		Debug.Log ("値送る");
+		int testInteger = 54321;
+		OSCHandler.Instance.SendMessageToClient ("myRemoteLocation", "192.168.2.100:12000", testInteger);
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Debug.Log ("値送るupdate");
+		int testInteger = 54321;
+		int random = Random.Range (0, 10000);
+		OSCHandler.Instance.SendMessageToClient ("myRemoteLocation", "192.168.2.100:12000", random);
+
+	}
+}
